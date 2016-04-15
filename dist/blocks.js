87968090faaf6b79ad9d292c98709a288366e8ed
hualuomoli <liubaoquan0831@163.com>
2016-04-15 17:40:04 +0800

    add base64 to image object

--- dist/blocks.js
+++ dist/blocks.js
@@ -33,6 +33,56 @@
     return s;
   }
 
+  // get file suffix
+  String.prototype.getFileSuffix = function () {
+    var str = this;
+    if (str === undefined || str.length === 0) {
+      return '';
+    }
+    var index = str.indexOf('.');
+    if (index === -1) {
+      return '';
+    }
+    return str.substring(index + 1);
+  }
+
+  // get file name
+  String.prototype.getFileName = function () {
+    var str = this;
+    if (str === undefined || str.length === 0) {
+      return '';
+    }
+    var index = str.indexOf('.');
+    if (index === -1) {
+      return '';
+    }
+    return str.substring(0, index);
+  }
+
+  // base64 to image
+  String.prototype.base64ToImage = function (filename) {
+
+    var base64 = this;
+
+    var bytes = window.atob(base64.split(',')[1]); //去掉url的头，并转换为byte
+    //处理异常,将ascii码小于0的转换为大于0
+    var ab = new ArrayBuffer(bytes.length);
+    var ia = new Uint8Array(ab);
+    for (var i = 0; i < bytes.length; i++) {
+      ia[i] = bytes.charCodeAt(i);
+    }
+
+    var type = 'image/' + filename.getFileSuffix;
+
+    var blob = new Blob([ab], {
+      type: type
+    });
+
+    blob.name = filename;
+
+    return blob;
+  }
+
   function validat(data, str) {
     if (!data || !str) {
       return false;
